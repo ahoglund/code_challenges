7eeada33e0e93425c97da2fdb6789db912886860
Andrew Hoglund <ahoglund@gmail.com>
2015-09-04 23:37:57 -0400

    added tests

---
+++ lib/streak_calculator.rb
@@ -0,0 +1,29 @@
+class StreakCalculator
+
+  def initialize(grades)
+    @grades = grades
+    @longest_streak = 0
+  end
+
+  attr_reader :longest_streak
+
+  def calculate
+    current_streak = []
+    (1..(@grades.length - 1)).each do |i|
+      if @grades[i] >= @grades[i - 1]
+        current_streak << @grades[i - 1]
+      else
+        current_streak << @grades[i - 1]
+        if current_streak.length > @longest_streak
+          @longest_streak = current_streak.length
+        end
+        current_streak = []
+      end
+    end
+    # for when the steak only goes upwards, never down
+    if @longest_streak == 0
+      current_streak << @grades.last
+      @longest_streak = current_streak.length
+    end
+  end
+end
---
+++ spec/streak_calculator_spec.rb
@@ -0,0 +1,23 @@
+require 'streak_calculator'
+
+RSpec.describe StreakCalculator, "#calculate" do 
+
+	expectations = [
+		[4, [9,7,8,2,5,5,8,7]],
+		[4, [1,2,3,4]],
+		[1, [5,4,3,2,1]],
+		[6, [1,7,2,5,6,9,11,11,1,6,1]],
+		[6, [6,7,5,3,6,6,8,9,11,3,3,3]],
+	]
+
+	expectations.each do |expectation|
+		streak, run = expectation[0], expectation[1]
+		context "with #{run} as run" do
+			it "should calculate the longest streak to #{streak}" do
+				streak_calculator = StreakCalculator.new(run)
+				streak_calculator.calculate
+		    expect(streak_calculator.longest_streak).to eq streak
+		  end
+		end
+	end
+end
