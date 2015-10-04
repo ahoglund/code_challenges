30f8ebd6f76222217aada059013ec7a1733e5bde
Andrew Hoglund <ahoglund@nyx.com>
2015-10-03 21:06:54 -0400

    way better design, just add stuff to STATS array and it will be available

--- lib/stats.rb
+++ lib/stats.rb
@@ -1,42 +1,30 @@
 class Stats
 
+  STATS = {
+    :two_plus_two =>     Proc.new { 2 + 2 },
+    :four_plus_four =>   Proc.new { 4 + 4 },
+    :three_times_two =>  Proc.new { 3 * 2 }
+  }
+
+  STATS.keys.each { |stat| self.class_eval { attr_reader stat } }
+
   def self.fetch(*opts)
     Stats.new(opts)
   end
 
   def initialize(opts)
-    @stats = opts
-    run_stats
+    run_stats(opts)
   end
 
-  def run_stats
-    @stats.any? ? run_selected_stats : run_all_stats
+  def run_stats(opts)
+    opts.any? ? run_selected_stats(opts) : run_all_stats
   end
 
-  def run_selected_stats
-    @stats.each { |stat| send("set_#{stat}".to_sym) }
+  def run_selected_stats(opts)
+    opts.each { |stat| instance_variable_set("@#{stat}", STATS[stat].call)  }
   end
 
   def run_all_stats
-    setter_methods = methods.grep /set_/
-    setter_methods.each do |method|
-      send(method)
-    end
-  end
-
-  attr_reader :stat_one
-  attr_reader :stat_two
-  attr_reader :stat_three
-
-  def set_stat_one
-    @stat_one = 1000
-  end
-
-  def set_stat_two
-    @stat_two = 2000
-  end
-
-  def set_stat_three
-    @stat_three = 3000
+    STATS.each { |stat,proc|  instance_variable_set("@#{stat}", proc.call) }
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Stats do
   subject { Stats }
   it { is_expected.to respond_to :fetch }
 
-  let(:all_stats) { [:stat_one, :stat_two, :stat_three] }
+  let(:all_stats) { [:two_plus_two, :four_plus_four, :three_times_two] }
 
   describe ".fetch" do 
     it "defaults to all stats" do
@@ -13,23 +13,22 @@ RSpec.describe Stats do
       all_stats.each do |stat|
         expect(stats).to respond_to stat
       end
-      binding.pry
     end
 
     it "responds to passed stats" do 
-      stats = Stats.fetch(:stat_one, :stat_two)
-      expect(stats).to respond_to(:stat_one)
-      expect(stats).to respond_to(:stat_two)
+      stats = Stats.fetch(:two_plus_two, :four_plus_four)
+      expect(stats).to respond_to(:two_plus_two)
+      expect(stats).to respond_to(:four_plus_four)
     end
 
     it "returns nil for non passed stat" do
-      stats = Stats.fetch(:stat_one)
-      expect(stats.stat_two).to eq nil
+      stats = Stats.fetch(:two_plus_two)
+      expect(stats.four_plus_four).to eq nil
     end
 
     it "returns a stat" do
-      stats = Stats.fetch(:stat_one)
-      expect(stats.stat_one).to eq 1000
+      stats = Stats.fetch(:two_plus_two)
+      expect(stats.two_plus_two).to eq 4
     end
   end
 end
