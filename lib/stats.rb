715017bf4c67264908248df6e052c78d5a94973a
Andrew Hoglund <ahoglund@nyx.com>
2015-10-03 21:40:41 -0400

    modularize

--- lib/stats.rb
+++ lib/stats.rb
@@ -1,30 +1,37 @@
-class Stats
-
-  STATS = {
-    :two_plus_two =>     Proc.new { 2 + 2 },
-    :four_plus_four =>   Proc.new { 4 + 4 },
-    :three_times_two =>  Proc.new { 3 * 2 }
-  }
-
-  STATS.keys.each { |stat| self.class_eval { attr_reader stat } }
-
-  def self.fetch(*opts)
-    Stats.new(opts)
+module Statistics
+ 
+  def all
+    {
+      :two_plus_two =>     Proc.new { 2 + 2 },
+      :four_plus_four =>   Proc.new { 4 + 4 },
+      :three_times_two =>  Proc.new { 3 * 2 }
+    }
   end
 
-  def initialize(opts)
-    run_stats(opts)
-  end
+  class Collection
 
-  def run_stats(opts)
-    opts.any? ? run_selected_stats(opts) : run_all_stats
-  end
+    extend Statistics
 
-  def run_selected_stats(opts)
-    opts.each { |stat| instance_variable_set("@#{stat}", STATS[stat].call)  }
-  end
+    all.keys.each { |stat| self.class_eval { attr_reader stat } }
 
-  def run_all_stats
-    STATS.each { |stat,proc|  instance_variable_set("@#{stat}", proc.call) }
+    def self.fetch(*opts)
+      Statistics::Collection.new(opts)
+    end
+
+    def initialize(opts)
+      run_stats(opts)
+    end
+
+    def run_stats(opts)
+      opts.any? ? run_selected_stats(opts) : run_all_stats
+    end
+
+    def run_selected_stats(opts)
+      opts.each { |stat| instance_variable_set("@#{stat}", self.class.all[stat].call)  }
+    end
+
+    def run_all_stats
+      self.class.all.each { |stat,proc|  instance_variable_set("@#{stat}", proc.call) }
+    end
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -1,33 +1,33 @@
 require 'stats'
 
-RSpec.describe Stats do
+RSpec.describe Statistics::Collection do
   
-  subject { Stats }
+  subject { Statistics::Collection }
   it { is_expected.to respond_to :fetch }
 
   let(:all_stats) { [:two_plus_two, :four_plus_four, :three_times_two] }
 
   describe ".fetch" do 
     it "defaults to all stats" do
-      stats = Stats.fetch
+      stats = Statistics::Collection.fetch
       all_stats.each do |stat|
         expect(stats).to respond_to stat
       end
     end
 
     it "responds to passed stats" do 
-      stats = Stats.fetch(:two_plus_two, :four_plus_four)
+      stats = Statistics::Collection.fetch(:two_plus_two, :four_plus_four)
       expect(stats).to respond_to(:two_plus_two)
       expect(stats).to respond_to(:four_plus_four)
     end
 
     it "returns nil for non passed stat" do
-      stats = Stats.fetch(:two_plus_two)
+      stats = Statistics::Collection.fetch(:two_plus_two)
       expect(stats.four_plus_four).to eq nil
     end
 
     it "returns a stat" do
-      stats = Stats.fetch(:two_plus_two)
+      stats = Statistics::Collection.fetch(:two_plus_two)
       expect(stats.two_plus_two).to eq 4
     end
   end
