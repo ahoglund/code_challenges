09ead64a8c8b53b972416ca94ff7c6b2b692bb1d
Andrew Hoglund <ahoglund@nyx.com>
2015-10-13 12:20:45 -0400

    refactored a bit based on similar code challenge

--- lib/streak_calculator.rb
+++ lib/streak_calculator.rb
@@ -3,28 +3,21 @@ class StreakCalculator
   def initialize(grades)
     @grades = grades
     @longest_streak = 0
+    @current_streak = 1
   end
 
   attr_reader :longest_streak
 
   def calculate
-    current_streak = []
-    (1..(@grades.length - 1)).each do |i|
-      if @grades[i] >= @grades[i - 1]
-        current_streak << @grades[i - 1]
-      else
-        current_streak << @grades[i - 1]
-        if current_streak.length > @longest_streak
-          @longest_streak = current_streak.length
-        end
-        # reset the streak
-        current_streak = []
+    return @longest_streak unless @grades.any?
+    (1..(@grades.length - 1)).each do |grade|
+      unless @grades[grade] >= @grades[grade - 1]
+        @longest_streak = @current_streak if @current_streak > @longest_streak
+        @current_streak = 0
       end
+      @current_streak += 1
     end
-    # for when the steak only goes upwards, never down
-    if @longest_streak == 0
-      current_streak << @grades.last
-      @longest_streak = current_streak.length
-    end
+    # for all upward and all same
+    @longest_streak = @current_streak if @longest_streak == 0
   end
 end
